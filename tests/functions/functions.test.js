ea3122a4d2c5b8afe379b2295fdb511dcdc725ad
Ben Shekhtman <benshekhtman@hotmail.com>
2022-05-20 14:41:08 -0400

    attmpted push'

--- tests/functions/functions.test.js
+++ tests/functions/functions.test.js
@@ -1,4 +1,4 @@
-import { sum } from "../../utils/functions";
+import { sum, fetchData } from "../../utils/functions";
 
 
 test('function sum should return sum of two numbers and to only run once', () => {
@@ -8,5 +8,34 @@ test('function sum should return sum of two numbers and to only run once', () =>
 
     expect(sumSim.mock.calls.length).toBe(1)
     expect(result).toEqual(4);
+
+})
+
+test('fetchData should return data from api when given valid url', async () => {
+    const fetchDataSim = jest.fn(fetchData);
+
+    const res = await fetchDataSim('https://jsonplaceholder.typicode.com/users')
+
+    expect(fetchDataSim).toBeCalledTimes(1);
+
+    expect(typeof (res) === 'object').toBeTruthy();
+    expect(res !== undefined).toBeTruthy();
+    expect(Object.keys(res).length > 0).toBeTruthy();
+
+});
+
+test('fetchData should returns errMessage when tried to run without a url', async () => {
+    const fetchDataSim = jest.fn(fetchData);
+
+    const res = await fetchDataSim();
+
+    expect(res).toBe('Url was not provided')
+});
+
+test('fetchData throws error when given an invalid url', async () => {
+    const fetchDataSim = jest.fn(fetchData);
+
+    await expect(fetchDataSim('https://jsonplsfsasraceholder.typicode.com/users')).rejects.toThrow(Error);
+
 
 })
