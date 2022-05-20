077f9dd5f4c46d002505fe7a16730f1450a6cd39
Ben Shekhtman <benshekhtman@hotmail.com>
2022-05-20 14:02:19 -0400

    updates'

--- utils/functions.js
+++ utils/functions.js
@@ -1,3 +1,17 @@
+import axios from "axios";
+
 export const sum = (a, b) => {
     return a + b
+}
+
+export const fetchData = async (url) => {
+    if (!url) return 'Url was not provided';
+
+    try {
+        const { data } = await axios.get(url);
+        return data;
+
+    } catch (err) {
+        throw new Error(err);
+    }
 }
